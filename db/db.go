41137894ead74fb8a7e7bae24a65051cd57537fe
xngln <davxliu@gmail.com>
2022-03-04 12:32:16 -0500

    fix style

--- db/db.go
+++ db/db.go
@@ -17,12 +17,13 @@ func InitDB() {
 	if err != nil {
 		log.Panic(err)
 	}
-
-	host := os.Getenv("PG_HOST")
-	port := os.Getenv("PG_PORT")
-	user := os.Getenv("PG_USER")
-	password := os.Getenv("PG_PASSWORD")
-	dbname := os.Getenv("PG_DBNAME")
+	var (
+		host     = os.Getenv("PG_HOST")
+		port     = os.Getenv("PG_PORT")
+		user     = os.Getenv("PG_USER")
+		password = os.Getenv("PG_PASSWORD")
+		dbname   = os.Getenv("PG_DBNAME")
+	)
 
 	psqlConn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
